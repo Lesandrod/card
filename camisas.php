e8461d5a1dcbe8c53dd98a1f592701c5d5583716
Lesandrod <lentrix12345@gmail.com>
2022-06-20 19:51:20 -0500

    first commit

---
+++ camisas.php
@@ -0,0 +1,67 @@
+
+<?php require_once "config/conexion.php"; 
+include("nav.php");?>
+  <header class="bg-light py-5">
+        <div class="container px-4 px-lg-5 my-5">
+            <div class="text-center text-white">
+                <img src="assets\img/logo.png" alt="...">
+                <p class="lead fw-normal text-white-50 mb-0">La mejor tienda de ropa</p>
+            </div>
+        </div>
+    </header>
+<section class="py-5">
+        <div class="container px-4 px-lg-5">
+            <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+                <?php
+                 $busqueda = $_GET['busqueda'];
+                $query = mysqli_query($conexion, "SELECT * FROM productos WHERE id_categoria = 12 AND nombre like '%$busqueda%' ");
+                $result = mysqli_num_rows($query);
+                if ($result > 0) {
+                    while ($data = mysqli_fetch_assoc($query)) { ?>
+
+                        <div class="col mb-5 productos">
+                            <div class="card h-100">
+                                <!-- Sale badge-->
+                                
+                                <!-- Product image-->
+                                <img class="card-img-top" src="assets/img/<?php echo $data['imagen']; ?>" alt="..." />
+                                <!-- Product details-->
+                                <div class="card-body p-4">
+                                    <div class="text-center">
+                                        <!-- Product name-->
+                                        <h5 class="fw-bolder"><?php echo $data['nombre'] ?></h5>
+                                        <p><?php echo $data['descripcion']; ?></p>
+                                        <!-- Product reviews-->
+                                        <div class="d-flex justify-content-center small text-warning mb-2">
+                                               
+                                        </div>
+                                        <!-- Product price-->
+                                        <span class="text-muted text-decoration-line-through"><?php echo $data['precio_normal'] ?></span>
+                                        <?php echo $data['precio_rebajado'] ?>
+                                    </div>
+                                </div>
+                                <!-- Product actions-->
+                                <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                    <div class="text-center"><a class="btn btn-outline-dark mt-auto agregar" data-id="<?php echo $data['id']; ?>" href="#">Agregar</a></div>
+                                </div>
+                            </div>
+                        </div>
+                <?php  }
+                } ?>
+
+            </div>
+        </div>
+    </section>
+    <!-- Footer-->
+    <footer class="py-5 bg-dark">
+        <div class="container">
+            <p class="m-0 text-center text-white">Copyright &copy; Your Website 2021</p>
+        </div>
+    </footer>
+    <!-- Bootstrap core JS-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- Core theme JS-->
+    <script src="assets/js/jquery-3.6.0.min.js"></script>
+    <script src="assets/js/scripts.js"></script>
+
+</html>
---
+++ nav.php
@@ -0,0 +1,56 @@
+<?php require_once "config/conexion.php"; ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Carrito de Compras</title>
+    <!-- Favicon-->
+    <link rel="icon" type="image/x-icon" href="assets\logo.ico" />
+    <!-- Bootstrap icons-->
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="assets/css/styles.css" rel="stylesheet" />
+    <link href="assets/css/estilos.css" rel="stylesheet" />
+</head>
+
+<body>
+    <a href="#" class="btn-flotante" id="btnCarrito">Carrito <span class="badge bg-success" id="carrito">0</span></a>
+    <!-- Navigation-->
+    <div class="containerr" id="nave" >
+        <nav class="navbar navbar-expand-lg navbar-light">
+            <div class="container-fluid">
+                
+                <a class="navbar-brand"  href="./"><img src="assets\img\logo.png" width="70px"  alt="..."></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                    
+                        <form class="d-flex ">
+
+                            <input id="busqueda" name="busqueda" class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                            <button class="btn btn-outline-success" name="enviar" id="enviar" type="submit"><i class="bi bi-search"></i></button>
+                        </form>
+                        
+                        <a  class="nav-link text-info" category="all">ㅤㅤ</a>
+                        <a href="./pantalones.php" class="nav-link text-info" category="all">Pantalones</a>
+                        <a href="./polos.php" class="nav-link text-info" category="all">Polos</a>
+                        <a href="./camisas.php" class="nav-link text-info" category="all">Camisas</a>
+                        <a href="./casacas.php" class="nav-link text-info" category="all">Casacas</a>
+                        <a href="./calzado.php" class="nav-link text-info" category="all">Calzado</a>
+                        
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
+    <!-- Header-->
+    
+</body>
+
+</html>
